e819c596023f4b83e558fa1294fdd7990276523a
mohabaks <mohabaks@protonmail.com>
2020-01-16 11:47:20 +0300

    Added Fixed XOR solution

--- Set1/challenge.py
+++ Set1/challenge.py
@@ -25,5 +25,35 @@ def hex_to_base64(hex_string):
     return base64_string
 
 
+def fixed_xor(string1, string2):
+    """Fixed XOR
+    This function takes two equal-length buffers and produces their XOR
+    combination.
+
+    Parameters
+    ----------
+    string1 : str
+        1st hex string buffer
+    string2 : str
+        2nd hex string buffer
+
+    Returns
+    -------
+    xor_result
+        Return XOR of two hex strings.
+
+    """
+    if len(string1) != len(string2):
+        print("Two hex strings are not of equal-length")
+        exit(1)
+    else:
+        unhexlify_string1 = binascii.unhexlify(string1)
+        unhexlify_string2 = binascii.unhexlify(string2)
+        xor_result = bytes(a ^ b for a, b in zip(unhexlify_string1,
+                                                 unhexlify_string2))
+
+        return binascii.hexlify(xor_result)
+
+
 if __name__ == '__main__':
     pass
